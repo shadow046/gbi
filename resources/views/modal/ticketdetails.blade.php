40781fd6ab57a1ab48a55444058e7ccc5bd28859
shadow046 <emorej046@gmail.com>
2021-09-01 13:41:01 +0800

    Hot Fixed

--- resources/views/modal/ticketdetails.blade.php
+++ resources/views/modal/ticketdetails.blade.php
@@ -113,7 +113,7 @@
             
             <hr>
             <div class="modal-footer">
-                @if (auth()->user()->roles->first()->name == "Agent")
+                @if (auth()->user()->hasanyrole('Agent', 'Manager'))
                     <input type="button" class="btn btn-secondary createBtn mr-auto" value="Create New Ticket">
                     <input type="button" class="btn btn-secondary EditBtn" id="EditBtn" value="Edit">
                 @endif
