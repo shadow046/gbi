8b75064a4a67ce69b87e231a8167fd5305f7265a
shadow046 <emorej046@gmail.com>
2021-12-10 13:07:03 +0800

    Update

--- config/database.php
+++ config/database.php
@@ -83,26 +83,6 @@ return [
             ]) : [],
         ],
 
-        'fsr' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('FSRHOST', '127.0.0.1'),
-            'port' => env('FSRPORT', '3306'),
-            'database' => env('FSRDATABASE', 'forge'),
-            'username' => env('FSRUSERNAME', 'forge'),
-            'password' => env('FSRPASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-
         'schem' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
